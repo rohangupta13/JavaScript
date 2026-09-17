02058a544eed7658e3996d825e23b372c459a6f5
rohangupta13 <rohankgupta890@gmail.com>
2026-09-17 11:30:05 +0530

    update again

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-let a=10;
+var a=100
 console.log(a)
+var a =20
+console.log(a)
+a=50
+console.log(a)
+
+let b=60
+console.log(b)
+b=90
+console.log(b)
+
+const e=60
+console.log(e)
+
+var c=89
+if(true)
+{
+    let x=10
+    let y=50
+    console.log(x,y,c)
+}
+console.log(c)
